ca3a6f96e897b9efe62ec49119ec7a98e2e503ee
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-04-07 14:50:07 +0400

    finalised methods

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -5,29 +5,37 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Selenium {
     public static WebDriver driver;
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","D:\\Acer\\AUA\\4th Year\\2nd Semester\\Software Testing\\chromedriver.exe");
-//        getSearchElement();
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","util/chromedriver.exe");
+        getSearchElement();
         getCareersElement();
-//        getHacksBlogElement();
-//        getReferencesJSElement();
+        getHacksBlogElement();
+        getReferencesJSElement();
+        getChangeThemeElement();
     }
 
-    public static void getSearchElement() throws InterruptedException {
+    public static void getSearchElement() {
         driver = new ChromeDriver();
         driver.get("https://developer.mozilla.org/en-US/");
         driver.manage().window().maximize();
 
-        WebElement searchInput = driver.findElement(By.xpath("//*[@id=\"hp-search-q\"]"));
+        WebElement searchInput = driver.findElement(By.id("top-nav-search-q"));
         searchInput.sendKeys(".reduce()");
         searchInput.sendKeys(Keys.ENTER);
-        Thread.sleep(1500);
 
-        WebElement firstResult = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/ul/li[1]/h3/a"));
-        firstResult.click();
+        String originalWindow = driver.getWindowHandle();
+        for (String windowHandle : driver.getWindowHandles()) {
+            if(!originalWindow.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        //opens first result
+        driver.findElement(By.cssSelector("#content > div.search-results > ul > li:nth-child(1) > h3 > a")).click();
     }
 
-    public static void getCareersElement() throws InterruptedException {
+    public static void getCareersElement() {
         driver = new ChromeDriver();
         JavascriptExecutor js = (JavascriptExecutor)driver;
 
@@ -37,37 +45,44 @@ public class Selenium {
         //select Careers from footer
         js.executeScript("document.querySelector(\"#nav-footer > div > div.page-footer-nav-col-1 > ul > li:nth-child(3) > a\").click();");
 
-        //this resize of page isnt working
-//        WebElement html = driver.findElement(By.tagName("html"));
-//        html.sendKeys(Keys.chord(Keys.CONTROL, Keys.SUBTRACT));
-//        html.sendKeys(Keys.chord(Keys.CONTROL, Keys.SUBTRACT));
+        //switch driver into the new tab
+        String originalWindow = driver.getWindowHandle();
+        for (String windowHandle : driver.getWindowHandles()) {
+            if(!originalWindow.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
 
-//        Thread.sleep(1500);
+        //Find careers in Berlin Location
+        WebElement locations = driver.findElement(By.xpath("//*[@id=\"id_location\"]"));
+        locations.click();
 
-            //select location in list
-//        Select location = new Select(driver.findElement(By.xpath("//*[@id=\"listings-filters\"]/div[1]")));
-//        location.selectByValue("Berlin Office");
-
-        //TODO: Find careers in Berlin Location
-
-//        WebElement locations = driver.findElement(By.xpath("//*[@id=\"id_location\"]"));
-//        locations.click();
-//
-//        WebElement berlin = driver.findElement(By.xpath("//*[@id=\"id_location\"]/option[3]"));
-//        berlin.click();
+        //select location in list
+        WebElement berlin = driver.findElement(By.xpath("//*[@id=\"id_location\"]/option[2]"));
+        berlin.click();
     }
 
-    public static void getHacksBlogElement() throws InterruptedException {
+    public static void getHacksBlogElement() {
         driver = new ChromeDriver();
         driver.get("https://developer.mozilla.org/en-US/");
         driver.manage().window().maximize();
+        driver.getWindowHandle();
 
         //open hacks blog page
         WebElement hacksBlog = driver.findElement(By.cssSelector("#nav-footer > div > div.page-footer-nav-col-1 > ul > li:nth-child(2) > a"));
         hacksBlog.click();
-        Thread.sleep(3500);
 
-        //write and in the search bar
+        //switch driver into the new tab
+        String originalWindow = driver.getWindowHandle();
+        for (String windowHandle : driver.getWindowHandles()) {
+            if(!originalWindow.contentEquals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+
+        //write and in the search bar of 2nd page
         WebElement searchInput = driver.findElement(By.xpath("/html/body/div/header/div/div[2]/form/input"));
         searchInput.sendKeys("javascript");
         searchInput.sendKeys(Keys.ENTER);
@@ -77,26 +92,39 @@ public class Selenium {
         firstBlog.click();
     }
 
-    public static void getReferencesJSElement() throws InterruptedException {
+    public static void getReferencesJSElement() {
         driver = new ChromeDriver();
         driver.get("https://developer.mozilla.org/en-US/");
         driver.manage().window().maximize();
         // Locating the Main Menu (Parent element)
         WebElement mainMenu = driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/div[2]/nav/ul/li[1]/a"));
 
-//Instantiating Actions class
+        //Instantiating Actions class
         Actions actions = new Actions(driver);
 
-//Hovering on main menu
+        //Hovering on main menu
         actions.moveToElement(mainMenu);
 
-// Locating the element from Sub Menu
+        // Locating the element from Sub Menu
         WebElement subMenu = driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/div[2]/nav/ul/li[1]/ul/li[4]/a"));
 
-//To mouseover on sub menu
+        //To mouseover on sub menu
         actions.moveToElement(subMenu);
 
-//build()- used to compile all the actions into a single step
+        //build()- used to compile all the actions into a single step
         actions.click().build().perform();
+    }
+    public static void getChangeThemeElement() {
+        driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+
+        driver.get("https://developer.mozilla.org/en-US/");
+        driver.manage().window().maximize();
+
+        WebElement themeButton = driver.findElement(By.className("theme-switcher-menu"));
+        themeButton.click();
+        js.executeScript("document.querySelector(\"#root > div > header > div > div.top-navigation-main > div.theme-switcher-menu > ul > li:nth-child(2) > button\").click();");
+
+
     }
 }
